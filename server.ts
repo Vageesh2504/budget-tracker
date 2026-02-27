ea43c36efa07b185e76ce7c564891b72924053b6
Vageesh2504 <hlvageesh2504@gmail.com>
2026-02-27 23:49:36 +0530

    Update server listen address to 171.76.219.158
    Change server listen address to a specific IP.

--- server.ts
+++ server.ts
@@ -432,7 +432,7 @@ app.get('/', (req, res) => {
   res.send('https://budget-tracker-l4s7.onrender.com')
 })
 
-  app.listen(PORT, "0.0.0.0/0", () => {
+  app.listen(PORT, "171.76.219.158/32", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
